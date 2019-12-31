9821b167a634c1822cee8af0b0ef1a42791e31f5
Derry Jatnika <derry.jatnika@gmail.com>
2019-12-31 14:54:33 +0700

    edit data

--- json_parsing.py
+++ json_parsing.py
@@ -1,26 +1,26 @@
-import json
 # %%
+
+import json
 # a Python object (dict):
-myfamily = {
-    "child1": {
-        "name": "Emil",
-        "year": 2004
+Customer = {
+    "cust1": {
+        "name": "Derry",
+        "member": 1
     },
-    "child2": {
-        "name": "Tobias",
-        "year": 2007
+    "cust2": {
+        "name": "Angga",
+        "member": 0
     },
-    "child3": {
-        "name": "Linus",
-        "year": 2011
+    "cust3": {
+        "name": "Lilis",
+        "member": 1
     }
 }
 
 # convert into JSON:
-y = json.dumps(myfamily)
+y = json.dumps(Customer)
 
 # the result is a JSON string:
 print(y)
-
 
 # %%
